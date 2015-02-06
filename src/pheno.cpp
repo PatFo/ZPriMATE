05b86b35b73be2af234687ea8ffe63b9ead62430
Patrick Foldenauer <foldenauer@pi104a.itp.uni-heidelberg.de>
2015-02-06 13:04:41 +0100

    Implemenated all quark species

--- src/pheno.cpp
+++ src/pheno.cpp
@@ -2,4 +2,17 @@
 
 using namespace pheno;
 
+
+//Implemenation of quark constructors
 up::up(float xlc, float xrc): fermion(1, 1, 2.3e-3, 2./3, xlc, xrc) { }
+
+down::down(float xlc, float xrc): fermion(1, -1, 4.8e-3, -1./3, xlc, xrc) { }
+
+charm::charm(float xlc, float xrc): fermion(2, 1, 1.275, 2./3, xlc, xrc) { }
+
+strange::strange(float xlc, float xrc): fermion(2, -1, 9.5e-2, -1./3, xlc, xrc) { }
+
+top::top(float xlc, float xrc): fermion(3, 1, 173.5, 2./3, xlc, xrc) { }
+
+bottom::bottom(float xlc, float xrc): fermion(3, -1, 4.18, -1./3, xlc, xrc) { }
+
--- src/pheno.h
+++ src/pheno.h
@@ -12,7 +12,36 @@ namespace pheno{
     public:
       up(float xlc=1., float xrc=1.);
   };
+  
+  class down : public fundamental::fermion{
+  /// Down-quark class that has default U(1)_X charges xlc=1, xrc=1
+    public:
+      down(float xlc=1., float xrc=1.);
+  };
+  
+  class charm : public fundamental::fermion{
+  /// Charm-quark class that has default U(1)_X charges xlc=1, xrc=1
+    public:
+      charm(float xlc=1., float xrc=1.);
+  };
+  
+  class strange : public fundamental::fermion{
+  /// Strange-quark class that has default U(1)_X charges xlc=1, xrc=1
+    public:
+      strange(float xlc=1., float xrc=1.);
+  };
+  
+  class top : public fundamental::fermion{
+  /// Top-quark class that has default U(1)_X charges xlc=1, xrc=1
+    public:
+      top(float xlc=1., float xrc=1.);
+  };
 
+  class bottom : public fundamental::fermion{
+  /// Bottom-quark class that has default U(1)_X charges xlc=1, xrc=1
+    public:
+      bottom(float xlc=1., float xrc=1.);
+  };
   
 }
 
