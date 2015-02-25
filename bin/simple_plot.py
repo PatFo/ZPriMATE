d21d061016d8e51ffbb53675ae8ee602ef4342dc
Patrick Foldenauer <patrick.foldenauer@gmx.de>
2015-02-25 16:29:04 +0100

    Adapted plot script to output of SpectrumScanner.

--- bin/simple_plot.py
+++ bin/simple_plot.py
@@ -3,22 +3,71 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Switches to enable plotting of cross sections
+plot_tot = 1
+plot_sm  = 1
+plot_int = 0
+plot_sig = 0
+
+
+
 #       This scrip needs the absolute path of the datafile as command line arguments 
 #       and then plots the three column data.
-
-
 f= sys.argv[1]
-  
-x, y1, y2 = np.genfromtxt(f, delimiter='\t\t').transpose()
 
+
+  
+#x, y1, y2 = np.genfromtxt(f, delimiter='\t\t').transpose()
+
+# Empty containers for data
+x  = []
+y1 = []
+y2 = [] 
+y3 = [] 
+y4 = []
+
+# Labels for plotting
+lx  = ''
+ly1 = ''
+ly2 = ''
+ly3 = '' 
+ly4 = ''
+
+# Read data file
+with open(f, 'r') as openedFile :
+    lno=0
+    for line in openedFile:
+        tab = line.split()
+        #First line are labels 
+        if lno==0:
+            lx =tab[0]
+            ly1=tab[1]
+            ly2=tab[2]
+            ly3=tab[3]
+            ly4=tab[4]
+        else:
+            x.append( np.double(tab[0]))
+            y1.append(np.double(tab[1]))
+            y2.append(np.double(tab[2]))
+            y3.append(np.double(tab[3]))
+            y4.append(np.double(tab[4]))
+        lno +=1
+
+# Create plots
 fig=plt.figure()
-plt.semilogy(x, y1, 'r',label='Total')
-plt.semilogy(x, y2, 'b',label='SM')
-#plt.semilogy(x, y3, 'g',label='SM + Signal')
+if plot_tot:
+    plt.semilogy(x, y1, 'r',label=ly1)
+if plot_int:
+    plt.semilogy(x, y2, 'y',label=ly2)
+if plot_sig:
+    plt.semilogy(x, y3, 'g',label=ly3)
+if plot_sm:
+    plt.semilogy(x, y4, 'b',label=ly4)
+
 plt.grid(True)
 plt.ylabel('CrossX [pb]')
 plt.xlabel('Ecm [GeV]')
-plt.title('Total CrossX')
+plt.title('Cross Section')
 plt.legend()
     
 f=f[:-3]+"pdf"
